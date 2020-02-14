006873212c3fe4277f9c2cff844ced40e6556251
Alexandr Monakhov <a.monahov@mail.ru>
2020-02-14 20:11:30 +0300

    Добавлен jshint

--- scripts/plugins/parallax.js
+++ scripts/plugins/parallax.js
@@ -1,10 +1,11 @@
+'use strict';
+
 function parallax(event) {
     let layers = document.querySelectorAll('.layer');
     layers.forEach(layer => {
         let speed = layer.getAttribute('data-speed');
-        layer.style.transform = `translateX(${event.clientX*speed/1000}px)`;
+        layer.style.transform = `translateX(${event.clientX * speed / 1000}px)`;
     });
-    console.dir(event);
 }
 
 document.addEventListener('mousemove', parallax);
