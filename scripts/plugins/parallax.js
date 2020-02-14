c7b488016aa93c434f77d8d6d06ffe9987032537
Alexandr Monakhov <a.monahov@mail.ru>
2020-02-14 20:52:56 +0300

    Доделан праллакс

--- scripts/plugins/parallax.js
+++ scripts/plugins/parallax.js
@@ -3,8 +3,9 @@
 function parallax(event) {
     let layers = document.querySelectorAll('.layer');
     layers.forEach(layer => {
-        let speed = layer.getAttribute('data-speed');
-        layer.style.transform = `translateX(${event.clientX * speed / 1000}px)`;
+        let speedX = layer.getAttribute('data-speedX');
+        let speedY = layer.getAttribute('data-speedY');
+        layer.style.transform = `translate(${event.clientX * speedX / 1000}px, ${event.clientY * speedY / 1000}px)`;
     });
 }
 
