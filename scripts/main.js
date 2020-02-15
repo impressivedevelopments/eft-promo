d8980215873b8d83667e2eed13f3035cd6eedf08
Alexandr Monakhov <a.monahov@mail.ru>
2020-02-15 23:15:23 +0300

    Доделан прелоадер

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,7 @@
 let bgTheme = document.querySelector('.factory-theme-bg'),
   objTheme = document.querySelector('.factory-theme-obj');
 
-const changeButton = document.getElementById('change-theme'),
-      spiner = document.querySelector('.spiner');
+const changeButton = document.getElementById('change-theme');
 
 document.body.addEventListener('contextmenu', (event) => {
   event.preventDefault();
@@ -15,11 +14,12 @@ changeButton.addEventListener('click', () => {
 });
 
 window.addEventListener('load', () => {
-    document.body.classList.add('loaded_hiding');
-    window.setTimeout(function () {
-      document.body.classList.add('loaded');
-      document.body.classList.remove('loaded_hiding');
-    }, 500);
+  document.body.classList.add('loaded_hiding');
+  document.body.style.backgroundColor = '#fff';
+  window.setTimeout(function () {
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
+  }, 500);
 });
 
 
