e039b07bcfe0951f17d23ff82887ff395757153f
Alexandr Monakhov <a.monahov@mail.ru>
2020-02-15 23:00:22 +0300

    Добавлен прелоадер

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,26 @@
 'use strict';
+let bgTheme = document.querySelector('.factory-theme-bg'),
+  objTheme = document.querySelector('.factory-theme-obj');
+
+const changeButton = document.getElementById('change-theme'),
+      spiner = document.querySelector('.spiner');
+
 document.body.addEventListener('contextmenu', (event) => {
   event.preventDefault();
 });
-
-let bgTheme = document.querySelector('.factory-theme-bg'),
-    objTheme = document.querySelector('.factory-theme-obj');
-
-const changeButton = document.getElementById('change-theme');
 
 changeButton.addEventListener('click', () => {
   bgTheme.classList.toggle('woods-theme-bg');
   objTheme.classList.toggle('woods-theme-obj');
 });
+
+window.addEventListener('load', () => {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+});
+
 
 
